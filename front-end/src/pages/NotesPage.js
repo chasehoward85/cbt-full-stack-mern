6a9826a126d94ee0576cf98de4464e4ef9323996
Chase Howard <chasehoward85@gmail.com>
2026-01-15 16:55:49 -0700

    Refactored notes into component

--- front-end/src/pages/NotesPage.js
+++ front-end/src/pages/NotesPage.js
@@ -1,24 +1,13 @@
-import { Link } from 'react-router-dom';
-
-const getWordCount = str => {
-	if(!str) return 0;
-
-	return str.split(' ').length;
-}
+import { NotesList } from '../components/NotesList';
 
 export const NotesPage = ({ notes }) => {
 	return (
 		<>
 		<h1>My Notes</h1>
-		{notes.map(notes => (
-			<div key={notes.id}>
-				<h3>{notes.title}</h3>
-				<p>{getWordCount(notes.content)} word(s)</p>
-				<Link to={`/notes/${notes.id}`}>
-					<button>View</button>
-				</Link>
-			</div>
-		))}
+		<NotesList notes={notes}/>
+		
+		<h1>Shared With Me</h1>
+		<NotesList notes={notes} />
 		</>
 	);
 }
