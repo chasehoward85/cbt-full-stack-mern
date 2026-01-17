82d905ff7497271508cff23e3c195e020577633e
Chase Howard <chasehoward85@gmail.com>
2026-01-16 17:11:50 -0700

    Created the create a note modal with styling

--- front-end/src/pages/NotesPage.js
+++ front-end/src/pages/NotesPage.js
@@ -1,19 +1,23 @@
-import { useContext } from 'react';
+import { useState, useContext } from 'react';
 
 import { NotesList } from '../components/NotesList';
+import { Modal } from '../components/Modal';
 
 import { NotesContext } from '../contexts/NotesContext';
 
 export const NotesPage = () => {
 	const { notes } = useContext(NotesContext);
-	
+
+	const [newNoteModalIsOpen, setNewNoteModalIsOpen] = useState(false);
+
 	return (
 		<>
+		<Modal isOpen={newNoteModalIsOpen} onRequestClose={() => setNewNoteModalIsOpen(false)}>
+			<h1>Create Note</h1>
+		</Modal>
 		<h1>My Notes</h1>
 		<NotesList notes={notes}/>
-		
-		<h1>Shared With Me</h1>
-		<NotesList notes={notes} />
+		<button onClick={() => setNewNoteModalIsOpen(true)}>+ Add a New Note</button>
 		</>
 	);
 }
