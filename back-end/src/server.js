2ec0b953867c46c9df12248521bc9aa14fb6fff5
Chase Howard <chasehoward85@gmail.com>
2026-01-20 17:39:05 -0700

    Added a database connection with environment secrets

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,5 +1,9 @@
 import express from 'express';
 import { v4 as uuid } from 'uuid';
+import { MongoClient } from 'mongodb';
+
+const DATABASE_URL = process.env.DATABASE_URL;
+const DATABASE_NAME = process.env.DATABASE_NAME;
 
 const app = express();
 app.use(express.json());
@@ -10,38 +14,46 @@ let notes = [{
 	content: '*Hello my dear friends*'
 }];
 
-app.get('/notes', (req, res) => {
-	res.json(notes);
-});
 
-app.post('/notes', (req, res) => {
-	const { title } = req.body;
+const start = async () => {
+	const client = await MongoClient.connect(`${DATABASE_URL}`);
+	const notesDb = client.db(`${DATABASE_NAME}`).collection('notes');
 
-	notes.push({
-		id: uuid(),
-		title,
-		content: '',
+	app.get('/notes', (req, res) => {
+		res.json(notes);
 	});
 
-	res.json(notes);
-});
+	app.post('/notes', (req, res) => {
+		const { title } = req.body;
 
-app.put('/notes/:noteId', (req, res) => {
-	const { noteId } = req.params;
-	const { title, content } = req.body;
+		notes.push({
+			id: uuid(),
+			title,
+			content: '',
+		});
 
-	notes = notes.map(note => note.id === noteId ? { id: noteId, title, content } : note);
+		res.json(notes);
+	});
 
-	res.json(notes);
-});
+	app.put('/notes/:noteId', (req, res) => {
+		const { noteId } = req.params;
+		const { title, content } = req.body;
 
-app.delete('/notes/:noteId', (req, res) => {
-	const { noteId } = req.params;
-	notes = notes.filter(note => note.id !== noteId);
+		notes = notes.map(note => note.id === noteId ? { id: noteId, title, content } : note);
 
-	res.json(notes);
-});
+		res.json(notes);
+	});
 
-app.listen(8080, () => {
-	console.log('Server is listening on port 8080');
-});
+	app.delete('/notes/:noteId', (req, res) => {
+		const { noteId } = req.params;
+		notes = notes.filter(note => note.id !== noteId);
+
+		res.json(notes);
+	});
+
+	app.listen(8080, () => {
+		console.log('Server is listening on port 8080');
+	});
+}
+
+start();
