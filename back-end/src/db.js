6853b63eb88bd349df351b8dc2e9e0d076369ab4
Chase Howard <chasehoward85@gmail.com>
2026-01-22 17:10:07 -0700

    Refactor database use to include user ownership

--- back-end/src/db.js
+++ back-end/src/db.js
@@ -4,12 +4,14 @@ const DATABASE_URL = process.env.DATABASE_URL;
 const DATABASE_NAME = process.env.DATABASE_NAME;
 
 let client = null;
-let notesDb = null;
+
+export let notesDb = null;
+export let usersDb = null;
 
 export const initializeDbConnection = async () => {
 	client = await MongoClient.connect(DATABASE_URL);
 
-	notesDb = client.db(DATABASE_NAME).collection('notes');
+	const db = client.db(DATABASE_NAME)
+	notesDb = db.collection('notes');
+	usersDb = db.collection('users');
 }
-
-export { notesDb };
