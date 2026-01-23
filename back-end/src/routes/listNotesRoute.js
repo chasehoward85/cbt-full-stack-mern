d9579a4b09fd1ebbedde1f5168e7290a3e04b17a
Chase Howard <chasehoward85@gmail.com>
2026-01-23 14:41:54 -0700

    Protect listing notes route to only show an authed user's notes

--- back-end/src/routes/listNotesRoute.js
+++ back-end/src/routes/listNotesRoute.js
@@ -1,15 +1,28 @@
+import * as admin from 'firebase-admin';
+
 import { usersDb, notesDb } from '../db';
 
 export const listNotesRoute = {
 	path: '/users/:userId/notes',
 	method: 'get',
 	handler: async(req, res) => {
-		const { userId } = req.params;
+		try {
+			const { authtoken } = req.headers;
+			const authUser = await admin.auth().verifyIdToken(authtoken);
 
-		const user = await usersDb.findOne({ id: userId });
-		
-		const notes = await Promise.all(user.notes.map(id => notesDb.findOne({ id })));
+			const { userId } = req.params;
 
-		res.json(notes);
+			if(authUser.uid !== userId) {
+				res.sendStatus(403);
+			}
+
+			const user = await usersDb.findOne({ id: userId });
+			
+			const notes = await Promise.all(user.notes.map(id => notesDb.findOne({ id })));
+
+			res.json(notes);
+		} catch(e) {
+			res.sendStatus(401);
+		}
 	}
 }
