ee0e2acc7afbbec287cad75a8a26802257b91e63
Chase Howard <chasehoward85@gmail.com>
2026-01-23 15:45:49 -0700

    Protect creating notes to only allow an authed user to create their notes

--- back-end/src/routes/createNoteRoute.js
+++ back-end/src/routes/createNoteRoute.js
@@ -1,3 +1,4 @@
+import * as admin from 'firebase-admin';
 import { v4 as uuid } from 'uuid';
 
 import { usersDb, notesDb } from '../db';
@@ -6,29 +7,41 @@ export const createNoteRoute = {
 	path: '/users/:userId/notes',
 	method: 'post',
 	handler: async (req, res) => {
-		const { userId } = req.params;
-		const { title } = req.body;
+		try {
+			const { authtoken } = req.headers;
+			const authUser = await admin.auth().verifyIdToken(authtoken);
 
-		const newNoteId = uuid();
+			const { userId } = req.params;
+			
+			if(authUser.uid !== userId) {
+				return res.sendStatus(403);
+			}
 
-		const newNote = {
-			id: newNoteId,
-			title,
-			content: '',
-			createdBy: userId,
+			const { title } = req.body;
+
+			const newNoteId = uuid();
+
+			const newNote = {
+				id: newNoteId,
+				title,
+				content: '',
+				createdBy: userId,
+			}
+
+			const result = await notesDb.insertOne(newNote);
+
+			await usersDb.updateOne({ id: userId }, {
+				$push: { notes: newNoteId },
+			});
+
+			const mongoId = result.insertedId;
+
+			res.json({
+				...newNote,
+				_id: mongoId,
+			});
+		} catch(e) {
+			res.sendStatus(401);
 		}
-
-		const result = await notesDb.insertOne(newNote);
-
-		await usersDb.updateOne({ id: userId }, {
-			$push: { notes: newNoteId },
-		});
-
-		const mongoId = result.insertedId;
-
-		res.json({
-			...newNote,
-			_id: mongoId,
-		});
 	}
 }
