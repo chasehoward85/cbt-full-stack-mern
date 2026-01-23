d9a9348053bae5fcfd2b8ee5150ead357b8371eb
Chase Howard <chasehoward85@gmail.com>
2026-01-23 15:35:05 -0700

    Protect updating notes to only allow an authed user to update their notes

--- back-end/src/routes/updateNoteRoute.js
+++ back-end/src/routes/updateNoteRoute.js
@@ -1,20 +1,33 @@
+import * as admin from 'firebase-admin';
+
 import { notesDb } from '../db';
 
 export const updateNoteRoute = {
 	path: '/notes/:noteId',
 	method: 'put',
 	handler: async (req, res) => {
-		const { noteId } = req.params;
-		const { title, content } = req.body;
+		try {
+			const { authtoken } = req.headers;
+			const authuser = await admin.auth().verifyIdToken(authtoken);
 
-		const result = await notesDb.findOneAndUpdate({ id: noteId }, {
-			$set: { title, content },
-		}, {
-			returnDocument: 'after',
-		});
+			const { noteId } = req.params;
+			const { title, content } = req.body;
 
-		const updatedNote = result;
+			const note = await notesDb.findOne({ id: noteId });
+			
+			if(note.createdBy !== authuser.uid) {
+				return res.sendStatus(403);
+			}
 
-		res.json(updatedNote);
+			const updatedNote = await notesDb.findOneAndUpdate({ id: noteId }, {
+				$set: { title, content },
+			}, {
+				returnDocument: 'after',
+			});
+
+			res.json(updatedNote);
+		} catch(e) {
+			res.sendStatus(401);
+		}
 	}
 }
