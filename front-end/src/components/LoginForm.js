2a8e3254bab9fda2149ef50fc833eeaf3f192d56
Chase Howard <chasehoward85@gmail.com>
2026-01-25 10:47:12 -0700

    Add Firebase error handling for logging in

--- front-end/src/components/LoginForm.js
+++ front-end/src/components/LoginForm.js
@@ -1,11 +1,19 @@
 import { useState } from 'react';
 
-export const LoginForm = ({ onSubmit }) => {
+const errorMessageMap = {
+	'Firebase: Error (auth/invalid-email).': 'Please enter a valid email',
+	'Firebase: Error (auth/invalid-credential).': 'Invalid email or password',
+	'Firebase: Error (auth/user-not-found).': 'An account does not exist with that email',
+	'Firebaes: Error (auth/wrong-password).': 'Incorrect password',
+}
+
+export const LoginForm = ({ error, onSubmit }) => {
 	const [email, setEmail] = useState('');
 	const [password, setPassword] = useState('');
 
 	return (
 		<>
+		{error && <p className="error">{errorMessageMap[error] || 'An error occurred'}</p>}
 		<input
 			className="full-width space-below"
 			type="email"
