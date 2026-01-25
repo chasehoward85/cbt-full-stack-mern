45dcdfb4223426a18f6beccc8b0ca1a3a2984566
Chase Howard <chasehoward85@gmail.com>
2026-01-25 11:02:37 -0700

    Add FIrebase error handling for creating an account

--- front-end/src/pages/CreateAccountPage.js
+++ front-end/src/pages/CreateAccountPage.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import axios from 'axios';
 import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
@@ -5,23 +6,31 @@ import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
 import { CreateAccountForm } from '../components/CreateAccountForm';
 
 export const CreateAccountPage = () => {
+	const [error, setError] = useState('');
+
 	const history = useHistory();
 
 	const createAccount = async (email, password, confirmPassword) => {
-		if(password === confirmPassword) {
+		try {
+			if(password !== confirmPassword) {
+				throw new Error('Passwords do not match');
+			}
+
 			const result = await createUserWithEmailAndPassword(getAuth(), email, password);
 			const token = await result.user.getIdToken();
 
 			await axios.post('/users', {}, { headers: { authtoken: token }});
 		
 			history.push('/notes');
+		} catch(e) {
+			setError(e.message);
 		}
 	}
 
 	return (
 		<div className="centered-container">
 			<h1 className="h-centered">Create Account</h1>
-			<CreateAccountForm onSubmit={createAccount}/>
+			<CreateAccountForm error={error} onSubmit={createAccount}/>
 			<Link
 				style={{ display: 'block' }}
 				className="h-centered"
