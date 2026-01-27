ea96531754ef3ad734fb663931e265219ae8f37a
Chase Howard <chasehoward85@gmail.com>
2026-01-27 14:35:56 -0700

    Display who is currently logged in with styling

--- front-end/src/components/NavBar.js
+++ front-end/src/components/NavBar.js
@@ -1,15 +1,27 @@
 import { Link } from 'react-router-dom';
 
+import { useUser } from '../hooks/useUser';
+
 import { LogoutButton } from './LogoutButton';
 
 export const NavBar = () => {
+	const { isLoading, user } = useUser();
+
 	return (
 		<nav>
 			<Link className="brand-logo" to="/notes">
 				<h1><span className="brand-highlight">NŌT</span>LAB</h1>
 			</Link>
 
-			<LogoutButton />
+			<span className="user-actions">
+				{isLoading
+					? <p>Loading...</p>
+					: user
+						? <p>Logged in as: {user.email}</p>
+						: null}
+
+				<LogoutButton />
+			</span>
 		</nav>
 	);
 }
