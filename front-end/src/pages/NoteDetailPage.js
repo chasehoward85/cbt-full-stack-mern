bdfca38ef7e3a1e0c5729f64f9d15b62ef28212a
Chase Howard <chasehoward85@gmail.com>
2026-05-01 22:30:30 -0600

    Create useNote.js hook

--- front-end/src/pages/NoteDetailPage.js
+++ front-end/src/pages/NoteDetailPage.js
@@ -1,85 +1,22 @@
-import { useState, useContext, useEffect } from 'react';
+import { useState } from 'react';
 import { useParams, useHistory } from 'react-router-dom';
 import ReactMarkdown from 'react-markdown';
-import socketIoClient from 'socket.io-client';
 
 import { NoteNotFoundPage } from './NoteNotFoundPage';
 
-import { NotesContext } from '../contexts/NotesContext';
-
-import { useUser } from '../hooks/useUser';
+import { useNote } from '../hooks/useNote';
 
 export const NoteDetailPage = ({ isOwner }) => {
-	const { isLoading } = useContext(NotesContext);
-	
 	const { noteId } = useParams();
-
-	const { user } = useUser();
 
 	const history = useHistory();
 
-	const [socket, setSocket] = useState(null);
+	const { note = {}, isNotFound, error, isLoading, updateTitle, updateContent } = useNote(noteId);
+	const { title = '', content = '', role = '' } = note;
 
 	const [isEditing, setIsEditing] = useState(false);
-	const [updatedTitle, setUpdatedTitle] = useState('');
-	const [updatedContent, setUpdatedContent] = useState('');
-	const [role, setRole] = useState('');
-	const [isNotFound, setIsNotFound] = useState(false);
 
 	const canEdit = role === 'edit';
-	
-	useEffect(() => {
-		const connectToSocket = async() => {
-			const socket = socketIoClient('http://127.0.0.1:8080', {
-				query: {
-					noteId,
-					token: await user.getIdToken(),
-				}
-			});
-
-			socket.on('initialNoteData', (note) => {
-				if(note) {
-					setUpdatedTitle(note.title);
-					setUpdatedContent(note.content);
-					setRole(note.role);
-				}
-				else {
-					setIsNotFound(true);
-				}
-			});
-
-			socket.on('noteUpdated', (updatedNote) => {
-				setUpdatedTitle(updatedNote.title);
-				setUpdatedContent(updatedNote.content);
-				setRole(updatedNote.role);
-			});
-
-			socket.on('error', errorMessage => {
-				history.push(`/error?message=${encodeURIComponent(errorMessage)}`);
-			});
-
-			setSocket(socket);
-		}
-
-		if(!isLoading && user) {
-			connectToSocket();
-		}
-	}, [noteId, isLoading, user, history]);
-
-	useEffect(() => {
-		if(socket) {
-			return () => socket.disconnect();
-		}
-	}, [socket]);
-
-	useEffect(() => {
-		if(isEditing && socket) {
-			socket.emit('updateNote', {
-				title: updatedTitle,
-				content: updatedContent,
-			});
-		}
-	}, [isEditing, socket, updatedTitle, updatedContent]);
 
 	if(isLoading) {
 		return <p>Loading</p>
@@ -88,6 +25,10 @@ export const NoteDetailPage = ({ isOwner }) => {
 	if(isNotFound) {
 		return <NoteNotFoundPage />
 	}
+
+	if(error) {
+		return <p className="error">ERROR: {error}</p>
+	}
 	
 	if(isEditing) {
 		return (
@@ -95,14 +36,14 @@ export const NoteDetailPage = ({ isOwner }) => {
 			<input
 				className="full-width space-below"
 				placeholder="Enter a title"
-				value={updatedTitle}
-				onChange={e => setUpdatedTitle(e.target.value)} />
+				value={title}
+				onChange={e => { updateTitle(e.target.value) }} />
 
 			<textarea
 				className="full-width space-below"
 				placeholder="Type your note here"
-				value={updatedContent}
-				onChange={e => setUpdatedContent(e.target.value)} />
+				value={content}
+				onChange={e => { updateContent(e.target.value) }} />
 
 			<button onClick={() => setIsEditing(false)}>Done</button>
 			</>
@@ -111,8 +52,8 @@ export const NoteDetailPage = ({ isOwner }) => {
 
 	return (
 		<>
-		<h1>{updatedTitle}</h1>
-		{updatedContent ? <ReactMarkdown>{updatedContent}</ReactMarkdown> : <p className="weak">This note currently has no content</p>}
+		<h1>{title}</h1>
+		{content ? <ReactMarkdown>{content}</ReactMarkdown> : <p className="weak">This note currently has no content</p>}
 		<div className="evenly-spaced">
 			{isOwner && <button onClick={() => history.push(`/sharing-settings/${noteId}`)}>Share</button>}
 			{(isOwner || canEdit) && <button onClick={() => setIsEditing(true)}>Edit</button>}
